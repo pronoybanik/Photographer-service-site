08cc7f7f84968f97c3e1995a058ee27dc9802934
PRONOY <pronoybanik82@gmail.com>
2022-11-09 21:20:52 +0600

    another server create

--- index.js
+++ index.js
@@ -3,15 +3,60 @@ const cors = require('cors');
 const app = express()
 const port = process.env.POST || 5000;
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
 
 app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lijbrwd.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    const serviceCollection = client.db('assignments-11').collection('services');
+    const reviewCollection = client.db('assignments-11').collection('review');
+
+    try {
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
+
+        app.post('/review', async (req, res) => {
+            const review = req.body;
+            // console.log(review);
+            const result = await reviewCollection.insertOne(review)
+            res.send(result)
+        })
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(err => console.log(err))
+
+
+
+
 app.get('/', (req, res) => {
     res.send('server site running now')
-})
+});
 
 app.listen(port, () => {
     console.log(`care server running on ${port}`);
-})
+});
